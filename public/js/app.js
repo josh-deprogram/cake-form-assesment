db92db24ddbdfa57a5f7cb891c5b156d23997808
josh-dbp <info@designbuildplay.co.uk>
2017-05-17 12:44:49 +1000

    Added form validation on Other celebration input box

--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,21 @@ require("../css/style.css");
 
     console.log('Hello cake world');
 
+    // Basic form field validaiton on the Celebration Other Input type
+    const validateInput = (e)=> {
+        // Grab the input values
+        const celebration = document.querySelector('input[name="celebration"]:checked').value;
+        const otherInput = document.getElementById('other_input').value;
+        console.log('check the form:', celebration, otherInput);
+        
+        if(celebration === 'celebration_other' && otherInput === '') {
+            alert('Please enter the Other Celebration !!');
+            e.preventDefault();
+            return false;
+        }
+    }
+    
+    const cakeform = document.getElementById('cakeform');
+    cakeform.addEventListener("submit", validateInput);
+
 })();
